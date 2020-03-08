750abe01041e1a145ae95f2915e1d99220696dd9
Arka Roy Chowdhury <arkaroyc14@gmail.com>
2020-03-08 18:53:46 +0530

    Add Dirac Clone Manually From https://github.com/Havoc-OS/android_packages_apps_Dirac

--- msm8953.mk
+++ msm8953.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    Dirac
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
